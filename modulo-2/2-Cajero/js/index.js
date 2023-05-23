52fe3d8f679c9e6d6245ff02b33d788930b19aec
vianeydzj <vianeydiju@hotmail.com>
2023-05-22 19:50:52 -0600

    cambios cajero

--- modulo-2/2-Cajero/js/index.js
+++ modulo-2/2-Cajero/js/index.js
@@ -1,23 +1,33 @@
+
+
 var cuentas =[
     {nombre: "Mali",saldo: 200,password: 'helloworld'},
     {nombre: "Gera",saldo: 290,password: '133t'},
     {nombre: "Maui",saldo: 67,password: '123'}
 ];
 
-
+//funcion para validar dator e ingresar a la pagina de inicio
 function login() {
 nombre = document.getElementById("nombre").value;
 password = document.getElementById("password").value;
 
     if (nombre == "Mali" && password == 'helloworld') {      
-        window.location = "inicio.html" 
-        aler
+        window.location= "2-Cajero\inicio-Mali.html";  
 } else if (nombre == "Gera" && password == '133t') {
-    window.location = "inicio.html";
+    
+
 } else if (nombre == "Maui" && password == '123') {
-    window.location = "inicio.html";
-} else {
-    alert("Datos incorrectos");
+    
+}else {
+     alert("Datos incorrectos");
 }
+
 }
+
+function consultarMonto() {
+ if (condition) {
+    
+ }
+}
+
 
